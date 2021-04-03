f36f008c5c028206585cf854895c9cf48e007e50
Bernadette92 <79193891+Bernadette92@users.noreply.github.com>
2021-04-03 12:48:48 +0200

    Add files via upload

---
+++ 37_Opdracht Basic arrow functions/script.js
@@ -0,0 +1,19 @@
+const ikRockArrowFunctions = () =>
+  console.log("Joe, ik rock de arrow functions!");
+
+ikRockArrowFunctions();
+
+const fivePlusSeven = () => 5 + 7;
+
+fivePlusSeven();
+
+const myFunction = (a, b) => a + b;
+
+myFunction(a, b);
+
+const addFive = (a) => a + 5;
+
+addFive(a);
+
+const createObject = () => ({ greeting: "hoi" });
+createObject();
